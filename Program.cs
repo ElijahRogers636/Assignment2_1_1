223e9d5198eed8d0644fbd9969926cba74612e9e
Elijah Rogers <ejrogersemail@gmail.com>
2024-11-11 11:29:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment2_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(123, "Elijah", "Rogers");
+
+            Console.WriteLine("ID: " + student1.studentId + "\nFirst Name: " + student1.studentFname + "\nLast Name: " + student1.studentLname);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Assignment2_1_1
+{
+    //Write a class: “Student” with private data members: StudentId ( int), StudentFname (string), StudentLname (string ), StudentGrade ( char ) and public properties for each data member.
+    //Instantiate the class and assign data to properties. Display the data back on console.
+    public class Student
+    {
+        private int StudentId;
+
+        public int studentId
+        {
+            get
+            {
+                return StudentId;
+
+            }
+            set
+            {
+                StudentId = value;
+
+            }
+        }
+
+        private string StudentFname;
+
+        public string studentFname
+        {
+            get
+            {
+                return StudentFname;
+
+            }
+            set
+            {
+                StudentFname = value;
+
+            }
+        }
+
+        private string StudentLname;
+
+        public string studentLname
+        {
+            get
+            {
+                return StudentLname;
+
+            }
+            set
+            {
+                StudentLname = value;
+
+            }
+        }
+
+        public Student(int StudentId, string StudentFname, string StudentLname)
+        {
+            this.StudentId = StudentId;
+            this.StudentFname = StudentFname;
+            this.StudentLname = StudentLname;
+        }
+
+	}
+}
